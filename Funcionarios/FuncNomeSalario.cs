8a255bc68eb4b2eaa0fc2aa2d06cc8a757c23d73
Maluh2020 <maluhmyth@gmail.com>
2020-09-08 15:14:09 -0300

    Calculando a media salarial dos funcuinarios

---
+++ Funcionarios/FuncNomeSalario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Funcionarios
+{
+    class FuncNomeSalario
+    {
+        public string Funcionarios;
+        public string nome;
+        public double salario;
+
+    }
+}
---
+++ Funcionarios/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Funcionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncNomeSalario f1 = new FuncNomeSalario();
+            FuncNomeSalario f2 = new FuncNomeSalario();
+
+            Console.WriteLine("Dados do primeiro Funcionário: ");
+            Console.Write("Nome: ");
+            f1.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo Funcionário: ");
+            Console.Write("Nome: ");
+            f2.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (f1.salario + f2.salario) / 2.0;
+
+            Console.WriteLine("Salário médio: a "  +  media.ToString ("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
